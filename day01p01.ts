ab5789ea2bcd786b057a4835cf31ba0e5bba67a5
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-15 19:24:09 +0100

    completed

--- day01p01.ts
+++ day01p01.ts
@@ -1,5 +1,9 @@
 import { readFileSync } from "node:fs";
 
+//=============================================================//
+//                        FIRST PART
+//=============================================================//
+
 function isBigger(numArr: number[], index: number): boolean {
 	if (numArr[index - 1] > numArr[index]) {
 		return true;
@@ -50,18 +54,6 @@ function sum(arr1: number[], arr2: number[]): number {
 	return sum;
 }
 
-let arr1 = [3, 4, 2];
-let arr2 = [4, 3, 5];
-
-let res = sum(arr1, arr2);
-// console.log(res);
-
-arr1 = [...arr1, 1, 3, 3];
-arr2 = [...arr2, 3, 9, 3];
-
-res = sum(arr1, arr2);
-// console.log(res);
-
 // Turn string into string[]
 const strIntoNumArr = (s: string): number[] => {
 	return s
@@ -83,28 +75,8 @@ function splitNumArrIntoTwoNumArr(numArr: number[]): [number[], number[]] {
 			numArr2.push(numArr[i]);
 		}
 	}
-	// console.log(`numArr1: ${numArr1}`);
-	// console.log(`numArr2: ${numArr2}`);
-
 	return [numArr1, numArr2];
 }
-
-let str = `
-82728   61150
-39850   94024
-`;
-
-const num = strIntoNumArr(str);
-// console.log(typeof num);
-// console.log(`number[]: ${num}`);
-
-const result = splitNumArrIntoTwoNumArr(num);
-// console.log(result);
-
-// console.log(typeof ["82728", "61150"]);
-
-// const s = strArr(str);
-// console.log(s);
 
 const finalResult = (string: string): number => {
 	const numbers = strIntoNumArr(string);
@@ -115,55 +87,51 @@ const finalResult = (string: string): number => {
 	return res;
 };
 
-let sumResult = finalResult(str);
-
-// console.log(sumResult);
-
-let input = `
-82728   61150
-39850   94024
-24609   43406
-24964   98661
-16230   17299
-`;
-
-input = `
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-`;
-
-let nArr = strIntoNumArr(input);
-console.log(nArr);
-
-let resArrs = splitNumArrIntoTwoNumArr(nArr);
-let arrOne = resArrs[0];
-let arrTow = resArrs[1];
-
-let sortedOne = sortInsertion(arrOne);
-let sortedTwo = sortInsertion(arrTow);
-
-console.log(sortedOne);
-console.log(sortedTwo);
-
-let sumRes = sum(resArrs[0], resArrs[1]);
-console.log(sumRes);
-
-let finalRes = finalResult(input);
-console.log(finalRes);
-
-// let output = finalResult(input);
-// console.log(output);
-
-// input = ``;
-// output = finalResult(input);
-// console.log(output);
-
-const fileContent = readFileSync("./day01p01.txt", "utf-8");
-console.log(fileContent);
-
+//! result of the first part
+let fileContent = readFileSync("./day01.txt", "utf-8");
 let resultFormFile = finalResult(fileContent);
-console.log(resultFormFile);
+console.log(`Part One: ${resultFormFile}`);
+
+//=============================================================//
+//                        SECOND PART
+//=============================================================//
+
+const isNumExists = (number: number, elementOfAnArr: number): boolean =>
+	number === elementOfAnArr ? true : false;
+
+const counter = (number: number, numArr: number[]): number => {
+	let count: number = 0;
+	for (const element of numArr) {
+		if (isNumExists(number, element)) {
+			count++;
+		}
+	}
+	return count;
+};
+
+const additionOfRepeatedElements = (
+	numArrOne: number[],
+	numArrTwo: number[]
+): number => {
+	let addSum: number = 0;
+	let count: number = 0;
+	let multRes: number = 0;
+
+	for (const element of numArrOne) {
+		count += counter(element, numArrTwo);
+		multRes = count * element;
+		addSum += multRes;
+		multRes = 0;
+		count = 0;
+	}
+	return addSum;
+};
+
+const strToTwoNumArr = (string: string): [number[], number[]] =>
+	splitNumArrIntoTwoNumArr(strIntoNumArr(string));
+
+let partTow = additionOfRepeatedElements(
+	strToTwoNumArr(fileContent)[0],
+	strToTwoNumArr(fileContent)[1]
+);
+console.log(`Part Two: ${partTow}`);
