ab990c8340c204ba46245cec424633db4d7e5e5d
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-18 21:07:28 +0100

    Done - p01

--- day02.ts
+++ day02.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "node:fs";
+
 //=============================================================//
 //                        FIRST PART
 //=============================================================//
@@ -5,8 +7,13 @@
 const isSmaller = (num1: number, num2: number): boolean =>
 	num1 > num2 ? true : false;
 
-const isDistanceValid = (num1: number, num2: number): boolean =>
-	Math.abs(num1 - num2) < 4 ? true : false;
+const isDistanceValid = (num1: number, num2: number): boolean => {
+	let num = Math.abs(num1 - num2);
+	if (num < 4 && num !== 0) {
+		return true;
+	}
+	return false;
+};
 
 const isIncreasingAndCorrect = (numArr: number[]): boolean => {
 	const length = numArr.length;
@@ -68,26 +75,59 @@ const input = [
 ];
 
 const numOfCorrects = (arrNumArr: number[][]): number => {
-	let amount: number = 0;
+	let result: number = 0;
 	// const length = arrNumArr.length;
 	for (const i of arrNumArr) {
 		if (isSmaller(i[0], i[1])) {
-			console.log(`isSmaller: ${isSmaller(i[0], i[1])}`);
+			// console.log(`isSmaller: ${isSmaller(i[0], i[1])}`);
 
 			if (isDecreasingAndCorrect(i)) {
-				amount++;
-				console.log(`amount in isSmaller: ${amount}`);
+				result++;
+				// console.log(`result in isSmaller: ${result}`);
 			}
 		} else {
-			console.log(`isSmaller: ${isSmaller(i[0], i[1])}`);
+			// console.log(`isSmaller: ${isSmaller(i[0], i[1])}`);
 			if (isIncreasingAndCorrect(i)) {
-				amount++;
-				console.log(`amount in isSmaller: ${amount}`);
+				result++;
+				// console.log(`result in isSmaller: ${result}`);
 			}
 		}
 	}
-	return amount;
+	return result;
 };
 
 let result = numOfCorrects(input);
 console.log(result);
+
+let strInput = `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+`;
+
+const arrayOfNumberArrays = (strInput: string): number[][] => {
+	return strInput
+		.trim() // Remove leading and trailing whitespace
+		.split("\n") // Split into rows
+		.map(
+			(row) => row.trim().split(/\s+/g).map(Number) // Remove spaces and convert to numbers
+		);
+};
+
+let arrOfArr = arrayOfNumberArrays(strInput);
+console.log(arrOfArr);
+
+result = numOfCorrects(arrOfArr);
+console.log(result);
+
+//! result of the first part
+let fileContent = readFileSync("./day02.txt", "utf-8");
+
+arrOfArr = arrayOfNumberArrays(fileContent);
+console.log(arrOfArr);
+
+result = numOfCorrects(arrOfArr);
+console.log(`Part One: ${result}`);
