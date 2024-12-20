c9a3d2486655fd41b0d8f71b36bce487ebc13cb3
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-20 19:47:11 +0100

    logic for the part 01 - complete

--- day03.ts
+++ day03.ts
@@ -3,6 +3,33 @@ const regex = /mul\(\d{1,3},\d{1,3}\)/g;
 const code =
 	"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-const matches = code.match(regex);
+const matches = code.match(regex) as RegExpExecArray;
 
 console.log(matches);
+
+const numArr = (input: RegExpMatchArray): number[][] => {
+	let output: number[][] = [];
+	if (input) {
+		const resultAsNumbers = input.map((match) => {
+			const numbers = match.match(/\d{1,3}/g); // Extract numbers from the string
+			return numbers ? numbers.map(Number) : []; // Convert to numbers
+		});
+		return resultAsNumbers;
+		// Example output: [[1, 4], [123, 5], [34, 56]]
+	}
+	return output;
+};
+
+let res = numArr(matches);
+console.log(res);
+
+const calculate = (numbers: number[][]): number => {
+	let total = 0;
+	for (const i of numbers) {
+		total += i[0] * i[1];
+	}
+	return total;
+};
+
+let result = calculate(res);
+console.log(result);
