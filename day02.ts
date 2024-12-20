0d90eb93de83fea7df64be48c003929ae9f49b45
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-20 18:49:23 +0100

    delete a line

--- day02.ts
+++ day02.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from "node:fs";
-import { disconnect } from "node:process";
 
 //=============================================================//
 //                        FIRST PART
